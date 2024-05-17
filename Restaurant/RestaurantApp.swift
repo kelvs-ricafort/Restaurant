c501d4d08cfbfdd7059084821447696f230f5eac
Kelvin <kelvs.r@icloud.com>
2024-05-17 17:19:10 +0800

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Kelvin on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
